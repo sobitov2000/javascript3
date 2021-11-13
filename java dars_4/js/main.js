c044e395593ca0d652e5ddfbeabcc7480df2287d
sobitov2000 <90320800+sobitov2000@users.noreply.github.com>
2021-11-13 21:11:08 +0500

    Add files via upload

---
+++ java dars_4/js/main.js
@@ -0,0 +1,33 @@
+// Math kutubhonasi
+
+// console.log(Math.max(10, 20));
+
+// var nomer = prompt("nomrde")
+// var nomerr = prompt("2nomerde")
+
+// console.log(Math.max(nomer, nomerr));
+
+// Math funksiyalari
+
+// pow - daraja
+// sqrt - ildiz
+// ceil - tepaga ko'tarib beradi, yaxlidlab beradi.
+// floor - paska tushiradi
+// round - yarimidan tepaga oladi
+// min - kichkinasini ko'rsatadi 10, 20
+// max - kattasini bilgilab beradi 20, 10
+
+// Array = Massiv
+
+// var number = ["salom", "bay"]
+// var number2 = number.pop()
+// console.log(number);
+
+// Arraay funksiyalari
+
+// push - ohiriga malumot kiritib beradi
+// unshift - Boshiga qo'shib beradi
+// shift - Boshitadi strimni olib tashidi
+// pop - ohiridan bittasini olib tashidi
+
+// Array qo
